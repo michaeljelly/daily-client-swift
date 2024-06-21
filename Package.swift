2ca1a91bc5e9f4a34f9423531f575675df31d16b
Michael Jelly <53475252+michaeljelly@users.noreply.github.com>
2024-06-21 01:10:10 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -24,12 +24,10 @@ let package = Package(
         .binaryTarget(
             name: "Daily",
             url: "https://www.daily.co/sdk/daily-client-ios-0.20.0.zip",
-            checksum: "1d15ab0037f7be3851c62a5052e70b6e645ddfa0a504ae4dcc094ab56be20b7e"
         ),
         .binaryTarget(
             name: "DailySystemBroadcast",
             url: "https://www.daily.co/sdk/daily-system-broadcast-client-ios-0.20.0.zip",
-            checksum: "319beb89b55921770366b301ed494b31d8144653b10d4d1c785780d42b68b811"
         ),
     ]
 )
